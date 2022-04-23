5d9453b7cac1fd9dc607194f999c3047b8a61dbf
Dejan <dej.tim@gmail.com>
2022-04-24 01:41:07 +0200

    Initialisation dans GITHUB

---
+++ prj-001_la-calculatrice/02-solution/main.py
@@ -0,0 +1,4 @@
+a = int(input("Entrez un premier nombre : "))
+b = int(input("Entrez un deuxième nombre : "))
+resultat = f"Le résultat de l'addition de {a} avec {b} est égal à {a + b}"
+print(resultat)
